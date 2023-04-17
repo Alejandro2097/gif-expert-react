1b08cf7d9b5e8b7dfb07b4da11a6219d5a8e6082
Alejandro2097 <alejo12381@hotmail.es>
2023-04-16 19:49:03 -0500

    todd

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,10 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* Titulo */}
         <h1>GifExpertApp</h1>
+        {/* Input */}
+        {/* Listado de gifs */}
     </>
   )
 }
