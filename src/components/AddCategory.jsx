7918c116882d7659430e6cb2698703cf0263909e
Alejandro2097 <alejo12381@hotmail.es>
2023-04-20 22:27:24 -0500

    prevent default

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -3,16 +3,22 @@ import { useState } from "react"
 
 export const AddCategory = () => {
     const [inputValue, setInputValue] = useState('One punch');
-    const onInputChange = (event) => {
-        console.log(event.target.value)
-        setInputValue(event.target.value)
+    const onInputChange = ({target}) => {
+        setInputValue(target.value)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputValue)
     }
   return (
-    <input
+    <form onSubmit={(event) => onSubmit(event)}>
+        <input
         type="text"
         placeholder="Buscar Gifs"
         value={inputValue}
         onChange={(event) => onInputChange(event)}
     />
+    </form>
+    
   )
 }
