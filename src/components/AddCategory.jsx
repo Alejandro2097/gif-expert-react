b37a6e438e3f5f9df440401d64f69f0f497ae185
Alejandro2097 <alejo12381@hotmail.es>
2023-04-20 22:23:33 -0500

    target

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,12 +2,17 @@ import { useState } from "react"
 
 
 export const AddCategory = () => {
-    const [inputValue, setInputValue] = useState('One punch')
+    const [inputValue, setInputValue] = useState('One punch');
+    const onInputChange = (event) => {
+        console.log(event.target.value)
+        setInputValue(event.target.value)
+    }
   return (
     <input
         type="text"
         placeholder="Buscar Gifs"
         value={inputValue}
+        onChange={(event) => onInputChange(event)}
     />
   )
 }
