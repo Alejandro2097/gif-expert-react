37f06e36e22567aa67b9b6cde798bb243693d710
Alejandro2097 <alejo12381@hotmail.es>
2023-05-03 22:03:52 -0500

    first test with the giphy api endpoint

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,10 @@
 export const GifGrid = ({category}) => {
+
+  const getGifs = async() => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=qg1pvYB3j3MRWnaXQfR3KHi67UuXJTch&q=${category}`;
+    const resp = await fetch(url);
+    console.log(resp);
+  }
   return (
     <>
         <h3>{category}</h3>
