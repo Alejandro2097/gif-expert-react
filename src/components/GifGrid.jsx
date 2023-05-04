3ddcea39a1624cbc6f823cc7ec27298bfbfe8f20
Alejandro2097 <alejo12381@hotmail.es>
2023-05-03 22:12:16 -0500

    desstructuring gif data

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,7 +3,14 @@ export const GifGrid = ({category}) => {
   const getGifs = async() => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=qg1pvYB3j3MRWnaXQfR3KHi67UuXJTch&q=${category}`;
     const resp = await fetch(url);
-    console.log(resp);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => ({
+      id: img.id,
+      title: img.title,
+      url: images.downsized_medium.url
+    }));
+    console.log(gifs);
   }
   getGifs();
   return (
