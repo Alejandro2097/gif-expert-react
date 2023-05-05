86cb17c332c1fd7cdf02a87f8471f2751196dcf6
Alejandro2097 <alejo12381@hotmail.es>
2023-05-04 22:08:43 -0500

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
 import { getGifs } from '../helpers/getGifs';
+import { useEffect } from 'react';
 
 export const GifGrid = ({category}) => {
   const [counter, setCounter] = useState(10);
   
-  getGifs(category);
-  setCounter(counter + 1);
+  useEffect(() => {
+    getGifs(category);
+  },[] )
+  
   return (
 
     <>
