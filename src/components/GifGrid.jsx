70e623ea653b68f7db0994125dc1e4d3a19b51cd
Alejandro2097 <alejo12381@hotmail.es>
2023-05-04 22:01:43 -0500

    set counter

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import { getGifs } from '../helpers/getGifs';
+
 export const GifGrid = ({category}) => {
+  const [counter, setCounter] = useState(10);
+
   getGifs(category);
+
   return (
 
     <>
         <h3>{category}</h3>
         <p>Hola mundo</p>
+        <button onClick={() => setCounter(counter + 1)}>1</button>
     </>
   )
 }
