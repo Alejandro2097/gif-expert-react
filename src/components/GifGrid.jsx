70b11d35df0e2aff3c99963e18072937261eb529
Alejandro2097 <alejo12381@hotmail.es>
2023-05-09 21:04:30 -0500

    odio java

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-
-import { getGifs } from '../helpers/getGifs';
 import { GifItem } from './GifItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
@@ -11,16 +7,19 @@ export const GifGrid = ({category}) => {
 
   
   return (
-
     <>
        <h3>{category}</h3>
-       
+       {
+        isLoading && (<h2>Cargando...</h2>)
+        
+       }
        <div className="card-grid">
           {
               images.map((image) => {
                 return (
                   <GifItem key={image.id}
-                           {...image}/>
+                           {...image}
+                  />
                 )
               })
           }
