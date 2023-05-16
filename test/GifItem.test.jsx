1c36d15ea9683d2e41986f3234c92578b6a052db
Alejandro2097 <alejo12381@hotmail.es>
2023-05-15 21:58:40 -0500

    hurt

--- test/GifItem.test.jsx
+++ test/GifItem.test.jsx
@@ -9,4 +9,19 @@ describe('Pruebas en <GifItem/>',  () => {
         const { container } = render(<GifItem url={url} title={title}/>)
         expect(container).toMatchSnapshot();
     });
+
+    test('debe de mostrar la imagen y la URL y ALT indicada', () => {
+        render(<GifItem title={title} url={url}/>);
+        // screen.debug();
+        // expect(screen.getByRole('img').src).toBe(url);
+        // expect(screen.getByRole('img').src).toBe(title);
+        const {src, alt} = screen.getByRole('img');
+        expect(src).toBe(url)
+        expect(src).toBe(alt)
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+        render(<GetItem title={title} url={url}/>);
+        expect(screen.getByText(title)).toBeTruthy();
+    })
 });
