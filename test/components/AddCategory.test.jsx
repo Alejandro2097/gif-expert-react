01b8c2487947fcb11a6c9b460980f782236d23bf
Alejandro2097 <alejo12381@hotmail.es>
2023-05-21 18:21:59 -0500

    omg sunday

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -1,9 +1,13 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { AddCategory } from "../../src/components";
 
 describe('Pruebas en <AddCategory/>', () => {
     test('debe de cambiar el valor de la caja de texto ', () => {
         render(<AddCategory onNewCategory={() => {}}/>);
+        const input = screen.getAllByRole('textbox');
+        fireEvent.input(input, {target: { value: 'Saitama'}});
+        expect(input.value).toBe('Saitama');
+        // screen.debug();
     });
 });
