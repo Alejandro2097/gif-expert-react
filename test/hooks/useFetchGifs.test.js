f33e29746e6e2ecfb98fea44c0d36582ba8049a8
Alejandro2097 <alejo12381@hotmail.es>
2023-05-21 22:43:05 -0500

    testing

--- test/hooks/useFetchGifs.test.js
+++ test/hooks/useFetchGifs.test.js
@@ -3,6 +3,9 @@ import { renderHook } from "@testing-library/react";
 describe('Pruebas en el hook useFetchGifs', () => {
     test('debe de regresar el estado inicial', () => {
         const { result } = renderHook(() => useFetchGifs('Dragon ball'));
-        console.log(result);
+        const  {images , isLoading } = result.current;
+
+        expect(images.length ).toBe(0);
+        expect(isLoading).toBeTruthy();
     });
 });
