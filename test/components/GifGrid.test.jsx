2988ffb2509a96df9314340085d78d6716e88bf7
Alejandro2097 <alejo12381@hotmail.es>
2023-05-21 22:21:27 -0500

    mockeando el fetch

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,9 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
+jest.mock('../../src/hooks/useFetchGifs');
 describe('pruebas en <GifGrid />', () =>{
     const category = 'One punch';
     test('debe de mostrar el loading inicialmente ', () =>{
-        render(<GifGrid/>);
+        render(<GifGrid category={category}/>);
+        expect(screen.getByText('Cargando...'));
+        expect(screen.getByText(category));
+        //screen.debug();
+    });
+    test('debe mostar items cuando se cargan las imagenes useFetchGifs', () => {
+        render(<GifGrid category={category}/>);
     });
 });
