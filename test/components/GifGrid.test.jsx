c4042eaddc76ca312101995e0ccbe51459b1e59d
Alejandro2097 <alejo12381@hotmail.es>
2023-05-21 22:28:58 -0500

    mocking the fetch response

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -6,12 +6,34 @@ jest.mock('../../src/hooks/useFetchGifs');
 describe('pruebas en <GifGrid />', () =>{
     const category = 'One punch';
     test('debe de mostrar el loading inicialmente ', () =>{
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
         render(<GifGrid category={category}/>);
         expect(screen.getByText('Cargando...'));
         expect(screen.getByText(category));
         //screen.debug();
     });
     test('debe mostar items cuando se cargan las imagenes useFetchGifs', () => {
+        const gifs = [
+            {
+                id: 'abc',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'
+            },
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: true
+        });
         render(<GifGrid category={category}/>);
+        //screen.debug();
+        expect(screen.getAllByRole('img').length).toBe(2);
     });
 });
