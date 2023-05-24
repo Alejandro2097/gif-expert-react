f73db2834cc4034e8d78f58e3e588b9c3b8fd682
Alejandro2097 <alejo12381@hotmail.es>
2023-05-23 19:35:17 -0500

    some final test

--- test/GifExpertApp.test.jsx
+++ test/GifExpertApp.test.jsx
@@ -2,7 +2,44 @@ import { render, screen } from "@testing-library/react";
 import { GifExpertApp } from "../src/GifExpertApp";
 
 describe('Pruebas en <GifExpertApp/', () => {
-    test('test sometihn', () => {
-        render(<GifExpertApp/>);
+     
+    const newCategory = 'Saitama';
+ 
+    //Primero Debe agregar categorias nuevas:
+    test('should Add new categories', () => {
+       const { container } = render(<GifExpertApp />);
+       const input = screen.getByRole('textbox'); 
+       const form = screen.getByRole('form'); 
+ 
+       //ahora disparo los eventos para agregar 3 categorias nuevas
+       fireEvent.input(input, { target: { value: newCategory } });
+       fireEvent.submit(form); //agrega Saitama
+ 
+       fireEvent.input(input, { target: { value: newCategory + '2' } });
+       fireEvent.submit(form); //agrega Saitama2
+ 
+       fireEvent.input(input, { target: { value: newCategory + '3' } });
+       fireEvent.submit(form);//agrega Saitama3
+ 
+       //Espero que se hallan agregado 3 categorías cuyos nombres aparecen en etiquetas h3
+       expect(screen.getAllByRole('heading', { level: 3 }).length).toBe(3);
+    });
+ 
+    //No debe agregar una categoría si ya existe
+    test('Should not add a repeated category', () => {
+        const { container } = render(<GifExpertApp />);
+        const input = screen.getByRole('textbox'); 
+        const form = screen.getByRole('form'); 
+ 
+        //disparo los eventos para agregar una categoría
+        fireEvent.input(input, { target: { value: newCategory } });
+        fireEvent.submit(form);//agrega Saitama
+ 
+        //Intento agregar la misma categoria
+        fireEvent.input(input, { target: { value: newCategory } });
+        fireEvent.submit(form); //intento agregar Saitama nuevamente
+ 
+        //espero que no agregue la misma categoría y sólo esté agregada la primera vez
+        expect(screen.getAllByRole('heading', { level: 3 }).length).toBe(1);
     });
 }); 
